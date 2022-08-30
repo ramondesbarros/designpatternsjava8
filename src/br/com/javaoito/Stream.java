6f87f91c2103d119ee97f67aa8263dad311bba29
Ramon Barros <ramondesbarros@gmail.com>
2022-08-29 22:49:37 -0300

    stream filter

--- src/br/com/javaoito/Stream.java
+++ src/br/com/javaoito/Stream.java
@@ -1,5 +1,28 @@
 package br.com.javaoito;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Stream {
 
+	public static void main(String[] args) {
+		
+		List<String> usuarios = Arrays.asList("Ramon", "Dave", "Hayley", "CJ Ramone", "Lisa", "Einstein");
+		
+		// Filter 
+		/***
+		 * uma operação intermediária que consiste em filtrar objetos com base em algum critério 
+		 * fornecido por parâmetro, esse critério é representado por uma função lambda.
+		 */
+		
+		List<String> collect = usuarios.stream()
+			.filter(u -> u.length() > 5)
+			.collect(Collectors.toList());
+		
+		collect.forEach(c -> System.out.println(c));
+		
+		// Return -> [Hayley, CJ Ramone, Einstein]
+
+	}
 }
